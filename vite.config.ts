c27e03488451c54f494432a3451a9b326bb97a91
IvanMonichev <monichevid@baltech.ru>
2025-04-22 11:21:19 +0300

    lib: Enhance app with UI library, routing, and config (6)
    - Integrate Ant Design for UI components
    - Configure routing using React Router
    - Add Prettier configuration
    - Add Redux Toolkit configuration
    - Update ESLint config
    - Update Typescript config
    - Add app version to the build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { version } from './package.json'
+import process from 'node:process'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(version)
+  },
+  server: {
+    port: Number(process.env.VITE_SERVER_PORT) || 3000
+  },
+  resolve: {
+    alias: {
+      '@': 'src'
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  },
 })
