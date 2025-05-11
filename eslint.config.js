0f10bf5610b3cc9176c53f3a140c92677bc80246
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-05-12 01:44:14 +0300

    стандартизировал параметры форматирования на backend и frontend

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -15,7 +16,8 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,7 +27,9 @@ export default tseslint.config(
       ],
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
-      'arrow-body-style': ['error', 'as-needed']
+      'arrow-body-style': ['error', 'as-needed'],
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn'
     }
   }
-)
+);
